521cebf1bccecec89fa3dd021cf97a68899127e0
Matheus Leite <matheusjoselfm@gmail.com>
2019-07-30 22:46:15 -0300

    add aula de funcoes passadas como parametro

---
+++ module01/funcoes/parametro/vigesimoOitavo.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func secret(nome string) (retorno string) {
+	retorno = fmt.Sprintf("Atenção caro %s.", nome) + "Eu sou Batman!"
+	return
+}
+func exec(funcao func(string) string, usuario, senha string) (retorno string) {
+	if usuario == "matheus" && senha == "123456" {
+		retorno = secret(usuario)
+	} else {
+		retorno = "Usuario Incorreto"
+	}
+	return
+}
+func main() {
+	var usuario, senha string
+	fmt.Println("Digite seu usuario")
+	fmt.Scanf("%s", &usuario)
+	fmt.Println("Digite sua senha")
+	fmt.Scanf("%s", &senha)
+	resultado := exec(secret, usuario, senha)
+	fmt.Println(resultado)
+}
