6a68c35298cb9af5e0a056181b29d02239b168af
Matheus Leite <matheusjoselfm@gmail.com>
2020-12-27 23:37:22 -0300

    adicionando espacamentos

--- module01/tipos/TipoPersonalizado/trigesimoSetimo.go
+++ module01/tipos/TipoPersonalizado/trigesimoSetimo.go
@@ -11,21 +11,20 @@ func (n Nota) entre(inicio, fim float64) bool {
 }
 
 func notaParaConceito(n Nota) string {
-	if n.entre(9, 10){
+	if n.entre(9, 10) {
 		return "A"
-	}else if n.entre(7,8.99){
+	} else if n.entre(7, 8.99) {
 		return "B"
-	}else if n.entre(3, 6.99){
+	} else if n.entre(3, 6.99) {
 		return "C"
 	}
 	return "D"
 }
 
-func main(){
+func main() {
 
 	fmt.Println("Nota: ", notaParaConceito(9.8))
 	fmt.Println("Nota: ", notaParaConceito(7.8))
 	fmt.Println("Nota: ", notaParaConceito(3.8))
-
 
 }
