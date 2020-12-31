d5926bf3417cd96b2306bb042a5f3d32e71f0162
Matheus Leite <matheusjoselfm@gmail.com>
2020-12-31 14:26:26 -0300

    aula 76 multiplexar

---
+++ module02/concorrencia/multiplexar/multiplexar.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/itzmatheus/html"
+)
+
+func encaminhar(origem <-chan string, destino chan string) {
+	for {
+		destino <- <-origem
+	}
+}
+
+// multiplexar - misturar (mensagens) num canal
+
+func juntar(entrada1, entrada2 <-chan string) <-chan string {
+
+	c := make(chan string)
+
+	go encaminhar(entrada1, c)
+	go encaminhar(entrada2, c)
+
+	return c
+
+}
+
+func main() {
+	c := juntar(
+		html.Titulo("https://www.cod3r.com.br", "https://www.google.com"),
+		html.Titulo("https://www.amazon.com", "https://www.youtube.com"),
+	)
+
+	fmt.Println(<-c, " | ", <-c)
+	fmt.Println(<-c, " | ", <-c)
+}
